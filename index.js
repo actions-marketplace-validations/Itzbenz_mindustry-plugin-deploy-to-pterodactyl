c52ff15569fc0699ae27abeda4fad9b09e5deec0
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2023-05-05 09:36:53 +0700

    w

--- index.js
+++ index.js
@@ -1,21 +1,75 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const Axios = require('axios');
+const fs = require('fs');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    try {
+        const artifactPath = core.getInput('artifactPath', {required: true});
+        const artifactName = artifactPath.split('/').pop();
+        const endpoint = core.getInput('endpoint', {required: true, trimWhitespace: true});
+        const token = core.getInput('token', {required: true, trimWhitespace: true});
+        const serverId = core.getInput('serverId', {required: true, trimWhitespace: true});
+        const doRestart = core.getInput('doRestart', {required: false, trimWhitespace: true}) === 'true';
+        const targetPath = core.getInput('targetPath', {
+            required: false,
+            trimWhitespace: true
+        }) || `config/mods/${artifactName}`;
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
 
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+        const buffer = fs.readFileSync(artifactPath);
+
+        const axios = Axios.create({
+            baseURL: endpoint,
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Accept": "application/json",
+            }
+        });
+        if (doRestart) {
+            //alert the horde (optional)
+            core.info("Alerting the horde");
+            try {
+                await axios.post(`/api/client/servers/${serverId}/command`, {
+                    command: "say Server is restarting in 10 seconds",
+                });
+            } catch (e) { /* empty */
+            }
+        }
+        core.info(`Uploading ${artifactName} to ${targetPath}`);
+        //upload file
+        /**
+         * curl "https://pterodactyl.file.properties/api/client/servers/1a7ce997/files/write?file=%2Feula.txt" \
+         *   -H 'Accept: application/json' \
+         *   -H 'Authorization: Bearer apikey' \
+         *   -X POST \
+         *   -b 'pterodactyl_session'='eyJpdiI6InhIVXp5ZE43WlMxUU1NQ1pyNWRFa1E9PSIsInZhbHVlIjoiQTNpcE9JV3FlcmZ6Ym9vS0dBTmxXMGtST2xyTFJvVEM5NWVWbVFJSnV6S1dwcTVGWHBhZzdjMHpkN0RNdDVkQiIsIm1hYyI6IjAxYTI5NDY1OWMzNDJlZWU2OTc3ZDYxYzIyMzlhZTFiYWY1ZjgwMjAwZjY3MDU4ZDYwMzhjOTRmYjMzNDliN2YifQ%3D%3D' \
+         *   -d '#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula).
+         * #You also agree that tacos are tasty, and the best food in the world.
+         * #Wed Dec 25 05:20:41 UTC 2019
+         * eula=true
+         * '
+         * @type {axios.AxiosResponse<any>}
+         */
+        const uploadFileResponse = await axios.post(`/api/client/servers/${serverId}/files/write`, buffer, {
+            params: {
+                file: targetPath,
+            }
+        });
+        console.log(uploadFileResponse.data);
+
+        if (doRestart) {
+            core.info("Restarting server");
+            const restartResponse = await axios.post(`/api/client/servers/${serverId}/power`, {
+                signal: "restart",
+            });
+            console.log(restartResponse.data);
+        }
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 run();
